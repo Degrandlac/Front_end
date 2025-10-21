dc6717427b760e256c02443b45e74798c60747fd
Degrandlac <kevinharerimana@outlook.com>
2025-10-21 21:09:30 +0200

    new https

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  IVR: "https://modern-rocks-jog.loca.lt",                 // IVR service
+  IVR: "https://many-papayas-burn.loca.lt",                 // IVR service
   STT: "https://b7ff27846359.ngrok-free.app/transcribe",  // Speech-to-text service
-  TTS: " https://396bb9e39c64.ngrok-free.app/speak",       // Text-to-speech service
+  TTS: "https://396bb9e39c64.ngrok-free.app/speak",       // Text-to-speech service
   CHAT: "https://c9fd2c0b3214.ngrok-free.app/chat",       // Chat / main backend server
 };
