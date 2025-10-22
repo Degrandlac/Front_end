7f342002b7feac7693f14f636b6296f202c68769
Degrandlac <kevinharerimana@outlook.com>
2025-10-22 10:14:04 +0200

    new

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  IVR: "https://silent-years-share.loca.lt",                 // IVR service
+  IVR: "https://empty-coats-pick.loca.lt",                 // IVR service
   STT: "https://ead23f74e2bd.ngrok-free.app/transcribe",  // Speech-to-text service
   TTS: "https://4701addf0997.ngrok-free.app/speak",       // Text-to-speech service
   CHAT: "https://e4a699b5cdd8.ngrok-free.app/chat",       // Chat / main backend server
