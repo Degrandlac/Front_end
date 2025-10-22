c385992e77b8e8bc15d5974ee86c27b90805e3f4
Degrandlac <kevinharerimana@outlook.com>
2025-10-22 09:31:09 +0200

    new url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  IVR: "https://many-papayas-burn.loca.lt",                 // IVR service
-  STT: "https://b7ff27846359.ngrok-free.app/transcribe",  // Speech-to-text service
-  TTS: "https://396bb9e39c64.ngrok-free.app/speak",       // Text-to-speech service
-  CHAT: "https://c9fd2c0b3214.ngrok-free.app/chat",       // Chat / main backend server
+  IVR: "https://metal-experts-eat.loca.lt",                 // IVR service
+  STT: "https://e1363d28e17f.ngrok-free.app/transcribe",  // Speech-to-text service
+  TTS: "https://de3361b0e183.ngrok-free.app/speak",       // Text-to-speech service
+  CHAT: "https://05942ff8a47f.ngrok-free.app/chat",       // Chat / main backend server
 };
