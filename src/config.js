8669bc6d68df35f2ac271e805ccf28f8bdc87dd1
Degrandlac <kevinharerimana@outlook.com>
2025-11-04 14:19:24 +0200

    new ip adress

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  IVR: "https://yellow-items-kneel.loca.lt",                 // IVR service
+  IVR: "https://rare-dingos-listen.loca.lt",                 // IVR service
   STT: "https://a72478ff9733.ngrok-free.app/transcribe",  // Speech-to-text service
   TTS: "https://59d9e51f1f14.ngrok-free.app/speak",       // Text-to-speech service
   CHAT: "https://fb0d7783e603.ngrok-free.app/chat",       // Chat / main backend server
