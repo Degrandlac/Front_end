69dd6b70d7474919323a9df9f76eeb0de01aebb7
Degrandlac <kevinharerimana@outlook.com>
2026-01-22 13:30:46 +0200

    new links

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  IVR: "https://bright-lamps-fetch.loca.lt",                 //8003 IVR service
+  IVR: "https://tall-seas-open.loca.lt",                 //8003 IVR service
   STT: "https://f3ba048c0c6e.ngrok-free.app/transcribe",          //8001 Speech-to-text service
   TTS: "https://cc176ea1c73d.ngrok-free.app/speak",       //8002 Text-to-speech service
   CHAT: "https://e356a16d9d9b.ngrok-free.app/chat",       // 8000 Chat / main backend server
