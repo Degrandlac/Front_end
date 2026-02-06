2dd28308813c69f2d1ebccc7425a9f8d0ffb7fab
Degrandlac <kevinharerimana@outlook.com>
2026-02-06 19:38:39 +0200

    new link

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  IVR: "https://tender-guests-beg.loca.lt",          // IVR service
+  IVR: "https://huge-points-crash.loca.lt",          // IVR service
   STT: "https://ce61-102-22-142-180.ngrok-free.app/transcribe", // Speech-to-text
   TTS: "https://be3a-102-22-142-180.ngrok-free.app/speak",     // Text-to-speech
   CHAT: "https://dcf0-102-22-142-180.ngrok-free.app/chat",     // Main backend
